b035d3798f38663eec87d43b1f20c9bdb120d692
Alex Popoutsis <ajpopoutsis@gmail.com>
2026-02-01 18:48:02 -0600

    Rework WhiteNoise app to Bedtime app

--- apps/Bedtime.cs
+++ apps/Bedtime.cs
@@ -1,30 +1,38 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HomeAssistantApps;
 
 [NetDaemonApp]
-public class WhiteNoise
+public class Bedtime
 {
-    public WhiteNoise(ILogger<WhiteNoise> logger, IScheduler scheduler, Entities entities)
+    public Bedtime(ILogger<Bedtime> logger, IScheduler scheduler, Entities entities)
     {
         var filename = "http://192.168.1.7:8754/Rain_Sounds_14_Hours.mp3";
-        var config = new List<WhiteNoiseConfig>
+        var bedtimeConfigs = new List<BedtimeConfig>
         {
-            new() {
+            new()
+            {
                 Entity = entities.InputBoolean.WhiteNoiseMorgan,
-                LinkedMediaPlayer = entities.MediaPlayer.MorgansRoomSpeaker
+                LinkedMediaPlayer = entities.MediaPlayer.MorgansRoomSpeaker,
+                BedtimeEntity = entities.InputBoolean.MorganBed,
+                Light = entities.Light.MorganSFunLampGroup
             },
-            new() {
+            new()
+            {
                 Entity = entities.InputBoolean.WhiteNoiseMarcy,
-                LinkedMediaPlayer = entities.MediaPlayer.MarcysRoomSpeaker
+                LinkedMediaPlayer = entities.MediaPlayer.MarcysRoomSpeaker,
+                BedtimeEntity = entities.InputBoolean.MarcyBed,
+                Light = entities.Light.MarcySFunLampGroup
             },
-            new() {
+            new()
+            {
                 Entity = entities.InputBoolean.WhiteNoise,
                 LinkedMediaPlayer = entities.MediaPlayer.MasterBedroomSpeaker
             }
         };
 
-        foreach (var cfg in config)
+        foreach (var cfg in bedtimeConfigs)
         {
             IDisposable? mediaPlayerSubscription = null;
             cfg.Entity
@@ -34,7 +42,8 @@ public class WhiteNoise
                     if (s.New?.State == "on")
                     {
                         logger.LogInformation("Starting white noise for {Entity}", cfg.Entity.EntityId);
-                        cfg.LinkedMediaPlayer.PlayMedia(new {
+                        cfg.LinkedMediaPlayer.PlayMedia(new
+                        {
                             media_content_type = "music",
                             media_content_id = filename
                         });
@@ -50,13 +59,23 @@ public class WhiteNoise
                         mediaPlayerSubscription?.Dispose();
                     }
                 });
+
+            cfg.BedtimeEntity?.StateChanges()
+                    .Where(s => s.New?.State == "on")
+                    .Subscribe(_ =>
+                    {
+                        logger.LogInformation("Bedtime started for {Entity}", cfg.BedtimeEntity.EntityId);
+                        cfg.Entity.TurnOn();
+                        cfg.Light?.TurnOff();
+                    });
         }
     }
-        
 }
 
-public class WhiteNoiseConfig
+public class BedtimeConfig
 {
     public required InputBooleanEntity Entity { get; set; }
     public required MediaPlayerEntity LinkedMediaPlayer { get; set; }
+    public InputBooleanEntity? BedtimeEntity { get; set; }
+    public LightEntity? Light { get; set; }
 }
