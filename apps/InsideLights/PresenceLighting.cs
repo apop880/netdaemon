957abe9f29e03b01191da28d9b4db05a772a78cf
Alex Popoutsis <ajpopoutsis@gmail.com>
2026-02-15 12:43:46 -0600

    Add vacation mode and lux sensor to PresenceLighting

--- apps/InsideLights/PresenceLighting.cs
+++ apps/InsideLights/PresenceLighting.cs
@@ -6,11 +6,20 @@ public class PresenceLighting
     public PresenceLighting(ILogger<PresenceLighting> logger, Entities entities, HomeMode homeMode)
     {
         homeMode.Current
-            .Where(mode => mode == HomeModeState.Away)
+            .Where(mode => mode is HomeModeState.Away or HomeModeState.Vacation)
             .Subscribe(_ =>
             {
                 logger.LogInformation("HomeMode changed to Away, turning off InsideLights");
                 entities.Light.InsideLights.TurnOff();
+            });
+        entities.Sensor.ThirdRealityInc3rsnl02043zIlluminance.StateChanges()
+            .Where(s => s.New?.State < 10)
+            .Subscribe(_ =>
+            {
+                if (homeMode.CurrentValue == HomeModeState.Away)
+                {
+                    entities.Light.KitchenSink.TurnOn(brightnessPct: 40);
+                }
             });
     }
 }
