e791c3631c1fafd37756f5a26a4c9264a9c7690c
Alex Popoutsis <ajpopoutsis@gmail.com>
2026-02-16 10:04:46 -0600

    PrescenceLighting: turn on kitchen and living room in evening

--- apps/InsideLights/PresenceLighting.cs
+++ apps/InsideLights/PresenceLighting.cs
@@ -1,10 +1,17 @@
+using NetDaemon.Extensions.Scheduler;
+
 namespace HomeAssistantApps;
 
 [NetDaemonApp]
 public class PresenceLighting
 {
-    public PresenceLighting(ILogger<PresenceLighting> logger, Entities entities, HomeMode homeMode)
+    public PresenceLighting(ILogger<PresenceLighting> logger, IScheduler scheduler, Entities entities, HomeMode homeMode)
     {
+        scheduler.ScheduleCron("0 12 * * *", () =>
+        {
+            entities.InputBoolean.EveningLightingTriggered.TurnOff();
+        });
+
         homeMode.Current
             .Where(mode => mode is HomeModeState.Away or HomeModeState.Vacation)
             .Subscribe(_ =>
@@ -28,6 +35,19 @@ public class PresenceLighting
                 if (homeMode.CurrentValue == HomeModeState.Away)
                 {
                     entities.Light.KitchenSink.TurnOn(brightnessPct: 40);
+                }
+            });
+        entities.Sensor.ThirdRealityInc3rsnl02043zIlluminance.StateChanges()
+            .Where(s => s.New?.State < 15)
+            .Subscribe(_ =>
+            {
+                if (homeMode.CurrentValue == HomeModeState.Home &&
+                    !entities.InputBoolean.EveningLightingTriggered.IsOn())
+                {
+                    entities.Light.KitchenTable.TurnOn();
+                    entities.Light.LivingCanLights.TurnOn();
+                    entities.InputBoolean.EveningLightingTriggered.TurnOn();
+                    logger.LogInformation("Evening lighting triggered due to low illuminance");
                 }
             });
     }
